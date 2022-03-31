2221b167f867b6bb6ca536f66e883f02d61a3833
wangqiao <qiao.wang@custcome-inc.com>
2022-03-31 16:48:31 +0800

    处理异常流程

--- src/index.ts
+++ src/index.ts
@@ -16,63 +16,68 @@ interface Inode {
 * 例：{a:1,c:null}，{a:[{a:1}]}
 */
 export function respondParamsFilter<T>(x: T, map: any = new WeakMap()): unknown {
-  // 判断是否是 undefined null
-  if (isUndef(x)) return '';
-  // 判断是普通类型 直接返回
-  if (isPlain(x)) return x;
-  let root: object | [] = {};
-  if (detectionType(x) === "object") {
-    root = {};
-  } else if (detectionType(x) === "array") {
-    root = [];
-  }
-  let loopList: Inode[] = [{
-    parent: root,
-    key: undefined,
-    data: x
-  }];
-  while (loopList.length) {
-    let node = loopList.pop();
-    const { data, parent, key }: any = node;
-    // 初始化赋值目标，key为undefined则拷贝到父元素，否则拷贝到子元素
-    let target = parent;
-    if (detectionType(key) !== "undefined") {
-      target = parent[key] = detectionType(data) === "object" ? {} : [];
+  try {
+    // 判断是否是 undefined null
+    if (isUndef(x)) return '';
+    // 判断是普通类型 直接返回
+    if (isPlain(x)) return x;
+    let root: object | [] = {};
+    if (detectionType(x) === "object") {
+      root = {};
+    } else if (detectionType(x) === "array") {
+      root = [];
     }
-    if (map.get(data)) {
-      parent[key] = map.get(data);
-      continue;
-    }
-    map.set(data, target);
-    if (detectionType(data) === "object") {
-      for (let key in data) {
-        if (data.hasOwnProperty(key)) {
+    let loopList: Inode[] = [{
+      parent: root,
+      key: undefined,
+      data: x
+    }];
+    while (loopList.length) {
+      let node = loopList.pop();
+      const { data, parent, key }: any = node;
+      // 初始化赋值目标，key为undefined则拷贝到父元素，否则拷贝到子元素
+      let target = parent;
+      if (detectionType(key) !== "undefined") {
+        target = parent[key] = detectionType(data) === "object" ? {} : [];
+      }
+      if (map.get(data)) {
+        parent[key] = map.get(data);
+        continue;
+      }
+      map.set(data, target);
+      if (detectionType(data) === "object") {
+        for (let key in data) {
+          if (data.hasOwnProperty(key)) {
+            // 不是object或者array
+            if (!isPlain(data[key])) {
+              loopList.push({
+                parent: target,
+                key,
+                data: data[key],
+              });
+            } else {
+              target[key] = isUndef(data[key]) ? '' : data[key];
+            }
+          }
+        }
+      } else if (detectionType(data) === "array") {
+        for (let i = 0; i < data.length; i++) {
           // 不是object或者array
-          if (!isPlain(data[key])) {
+          if (!isPlain(data[i])) {
             loopList.push({
               parent: target,
-              key,
-              data: data[key],
+              key: i,
+              data: data[i],
             });
           } else {
-            target[key] = isUndef(data[key]) ? '' : data[key];
+            target[i] = isUndef(data[i]) ? '' : data[i];
           }
         }
       }
-    } else if (detectionType(data) === "array") {
-      for (let i = 0; i < data.length; i++) {
-        // 不是object或者array
-        if (!isPlain(data[i])) {
-          loopList.push({
-            parent: target,
-            key: i,
-            data: data[i],
-          });
-        } else {
-          target[i] = isUndef(data[i]) ? '' : data[i];
-        }
-      }
     }
+    return root;
+  } catch (error) {
+    console.warn('error', error)
+    return x;
   }
-  return root;
 }
